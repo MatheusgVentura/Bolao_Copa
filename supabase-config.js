a83f90c246aa5cfd4f234ac9838a2e1dbb51a8cc
Matheus Ventura <matheusgventura10@gmail.com>
2026-07-08 15:43:17 -0300

    Re-adiciona supabase-config.js (apenas URL + anon key publicas)
    A senha do admin foi removida e e gerenciada pelo Supabase Auth.
    O arquivo agora contem apenas dados publicos (URL, anon key, URL dos
    jogos oficiais), seguros para estar no repositorio.

---
+++ supabase-config.js
@@ -0,0 +1,5 @@
+window.SUPABASE_CONFIG = {
+  url: "https://wjvbwklrcfqehppypjje.supabase.co",
+  anonKey: "sb_publishable_gEPUfz51yiNn4-4fd8c2HQ_-SNkmUI9",
+  officialMatchesUrl: "https://raw.githubusercontent.com/openfootball/worldcup.json/master/2026/worldcup.json"
+};
